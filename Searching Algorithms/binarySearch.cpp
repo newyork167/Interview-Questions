691b9d3c7a862794c11b133c566ce613b7e07c83
newyork167 <newyork167@gmail.com>
2014-12-11 09:12:46 -0500

    Updated

--- Searching Algorithms/binarySearch.cpp
+++ Searching Algorithms/binarySearch.cpp
@@ -6,8 +6,8 @@ int chop(int, int*, int);
 int main(int argc, char** argv){
 	int array[10] = {1,2,3,4,5,6,7,8,9,10};
 	std::cout << chop(8, array, sizeof(array)/sizeof(array[0])) << std::endl;
-    std::cout << chop(1, array, sizeof(array)/sizeof(array[0])) << std::endl;
-    std::cout << chop(3, array, sizeof(array)/sizeof(array[0])) << std::endl;
+	std::cout << chop(1, array, sizeof(array)/sizeof(array[0])) << std::endl;
+	std::cout << chop(3, array, sizeof(array)/sizeof(array[0])) << std::endl;
 }
 
 int chop(int x, int* array, int size){
@@ -15,10 +15,6 @@ int chop(int x, int* array, int size){
 	
 	while(true){
 		int y = floor((min + max) / 2);
-		
-		//std::cout << "Y: " << y << std::endl;
-		//std::cout << "min: " << min << std::endl;
-		//std::cout << "max: " << max << std::endl;
 		
 		if(min > max)
 			return -1;
