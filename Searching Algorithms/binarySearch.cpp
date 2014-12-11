c304db2133daec27c0cc070f423b8cfa84618a09
newyork167 <newyork167@gmail.com>
2014-12-11 15:04:25 -0500

    Updated

--- Searching Algorithms/binarySearch.cpp
+++ Searching Algorithms/binarySearch.cpp
@@ -5,9 +5,10 @@ int chop(int, int*, int);
 
 int main(int argc, char** argv){
 	int array[10] = {1,2,3,4,5,6,7,8,9,10};
-	std::cout << chop(8, array, sizeof(array)/sizeof(array[0])) << std::endl;
-	std::cout << chop(1, array, sizeof(array)/sizeof(array[0])) << std::endl;
-	std::cout << chop(3, array, sizeof(array)/sizeof(array[0])) << std::endl;
+	int sizeOfArray = sizeof(array)/sizeof(array[0]);
+	std::cout << chop(8, array, sizeOfArray) << std::endl;
+	std::cout << chop(1, array, sizeOfArray) << std::endl;
+	std::cout << chop(3, array, sizeOfArray) << std::endl;
 }
 
 int chop(int x, int* array, int size){
