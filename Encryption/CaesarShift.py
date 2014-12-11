6a1aa51fdf5bf1a559116537b60506195a8659db
newyork167 <newyork167@gmail.com>
2014-12-11 14:57:58 -0500

    Updated Caesar shift to take negative values

--- Encryption/CaesarShift.py
+++ Encryption/CaesarShift.py
@@ -2,9 +2,13 @@ import sys
 
 def CaesarShift(string, inc):
 	newString = ""
+	
+	if inc < 0:
+		inc = 26 - (-inc % 30)
+	elif inc > 26:
+		inc = inc % 26
+	
 	for x in string:
-		if inc > 26:
-			inc = inc % 26
 		y = ord(x) + inc
 		if y >= 91 and (y - inc) <= 91 :
 			y = 65 + (y - 91)
