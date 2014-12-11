62a4bd450010a29bbf1ed14ac0b3dc24d7d879e7
newyork167 <newyork167@gmail.com>
2014-12-10 19:58:29 -0500

    More awesomeness in the python

--- FizzBuzz/fizzBuzz.py
+++ FizzBuzz/fizzBuzz.py
@@ -1,13 +1,7 @@
 def fizzBuzz():
-	for x in range(1,101):
-        fizzBuzz = ""
-		
-		fizzBuzz += "fizz" if x % 3 == 0
-		fizzBuzz += "buzz" if x % 5 == 0
-		
-        if len(fizzBuzz) != 0:
-            print(fizzBuzz)
-		else:
-			print(x)
+    for x in range(1,101):
+		fizzBuzz = "fizz" if x % 3 == 0 else ""
+		fizzBuzz += "buzz" if x % 5 == 0 else ""
+		print(fizzBuzz if len(fizzBuzz) != 0 else x)
 			
 fizzBuzz()
