c68127e8307258beddd74fe935463b23ecf58f5b
newyork167 <newyork167@gmail.com>
2014-12-10 19:35:53 -0500

    Updated for more awesomeness

--- FizzBuzz/fizzBuzz.cpp
+++ FizzBuzz/fizzBuzz.cpp
@@ -1,13 +1,27 @@
 #import <iostream>
+#import <string>
 
-void fizzBuzz(){
+void fizzBuzz(int upperLimit){
 	int counter = 1;
 	
-	for(;counter <= 100; counter++){
-		std::cout << (counter % 3 == 0 && counter % 5 == 0 ? "fizzbuzz" : (counter % 3 == 0 ? "fizz" : (counter % 5 == 0 ? "buzz" : std::to_string(counter)))) << std::endl;
+	for(;counter <= upperLimit; counter++){
+        std::string toPrint = "";
+        
+        // New version only requires two modulo operations
+        counter % 3 == 0 ? toPrint += "fizz" : "";
+        counter % 5 == 0 ? toPrint += "buzz" : "";
+        
+        std::cout << (toPrint.length() == 0 ? std::to_string(counter) : toPrint) << std::endl;
+        
+        // Previous way I thought was awesome
+		//std::cout << (counter % 3 == 0 && counter % 5 == 0 ? "fizzbuzz" : (counter % 3 == 0 ? "fizz" : (counter % 5 == 0 ? "buzz" : std::to_string(counter)))) << std::endl;
 	}
 }
 
+// This does not check to ensure the argument is a number, this is just made to be fast
 int main(int argc, char** argv){
-	fizzBuzz();
+	if(argc < 2)
+		fizzBuzz(100);
+	else
+		fizzBuzz(atoi(argv[1]));
 }
