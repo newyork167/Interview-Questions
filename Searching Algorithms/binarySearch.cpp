3296f7178f6ae7c02714c30662dcf1b2cc31ba76
newyork167 <newyork167@gmail.com>
2014-12-11 09:11:45 -0500

    Code Kata source updated

---
+++ Searching Algorithms/binarySearch.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cmath>
+
+int chop(int, int*, int);
+
+int main(int argc, char** argv){
+	int array[10] = {1,2,3,4,5,6,7,8,9,10};
+	std::cout << chop(8, array, sizeof(array)/sizeof(array[0])) << std::endl;
+    std::cout << chop(1, array, sizeof(array)/sizeof(array[0])) << std::endl;
+    std::cout << chop(3, array, sizeof(array)/sizeof(array[0])) << std::endl;
+}
+
+int chop(int x, int* array, int size){
+	int min = 0, max = size;
+	
+	while(true){
+		int y = floor((min + max) / 2);
+		
+		//std::cout << "Y: " << y << std::endl;
+		//std::cout << "min: " << min << std::endl;
+		//std::cout << "max: " << max << std::endl;
+		
+		if(min > max)
+			return -1;
+		
+		if(array[y] > x)
+			max = y;
+		else if(array[y] < x)
+			min = y;
+		else
+			return y;
+	}
+	
+}
