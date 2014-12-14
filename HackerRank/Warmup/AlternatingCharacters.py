0189f801def70622c643f4054878430c324b21f6
newyork167 <newyork167@gmail.com>
2014-12-14 10:48:55 -0500

    Made better algorithm

--- HackerRank/Warmup/AlternatingCharacters.py
+++ HackerRank/Warmup/AlternatingCharacters.py
@@ -5,13 +5,26 @@ def remove(str, pos):
 
 for case in range(0, cases):
 	count = 0
-	string = "AABBABABB"
+	string = input()
+	points = []
 	x = 0
 	y = len(string)
-	while x < y:
-		while(x + 1 < y and string[x] == string[x+1]):
-			string = remove(string, x+1)
-			y -= 1
+	
+	# New code runs in O(n + m) much better than the O(nm)
+	for x in range(0, y):
+		if x + 1 < y and string[x] == string[x+1]:
+			points.append(x)
 			count += 1
-		x += 1
+	
+	for point in points:
+		string = string[:point] + string[(point+1):]
+	
+	
+	# Initial attempt runs in O(nm) failing one of the tests due to timeout
+	#while x < y:
+	#	while(x + 1 < y and string[x] == string[x+1]):
+	#		string = remove(string, x+1)
+	#		y -= 1
+	#		count += 1
+	#	x += 1
 	print(count)
