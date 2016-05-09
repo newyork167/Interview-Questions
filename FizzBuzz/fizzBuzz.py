1f78ac86c6ddae751d5459ed9116d94e4af60420
Cody Dietz <newyork167@gmail.com>
2016-05-09 14:24:16 -0400

    Update fizzBuzz.py

--- FizzBuzz/fizzBuzz.py
+++ FizzBuzz/fizzBuzz.py
@@ -3,5 +3,9 @@ def fizzBuzz():
 		fizzBuzz = "fizz" if x % 3 == 0 else ""
 		fizzBuzz += "buzz" if x % 5 == 0 else ""
 		print(fizzBuzz if len(fizzBuzz) != 0 else x)
+
+def fizzBuzz2():
+    # For an implementation using list comprehension, hopefully a more compact will be coming soon
+    ["fizzbuzz" if (x % 3 ==0 and x % 5 == 0) else "fizz" if x % 3 ==0 else "buzz" if x % 5 ==0 else x for x in range(1,101)]
 			
 fizzBuzz()
